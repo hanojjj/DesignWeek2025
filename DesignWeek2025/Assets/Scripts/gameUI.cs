5e59e5812a0025ded02931862c9fb0ddb5997c99
Jonah Cappa <jonahcappaofficial@gmail.com>
2025-02-13 17:28:32 -0500

    add timer to UI

--- DesignWeek2025/Assets/Scripts/gameUI.cs
+++ DesignWeek2025/Assets/Scripts/gameUI.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Xml;
+//using System.Xml;
 
 
 public class gameUI : MonoBehaviour
 {
 
     public TextMeshProUGUI time;
-
+    public Death death; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        time.text = "Time: " + death.timeRemaining;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time.text = "Time: " + death.timeRemaining;
+
     }
 }
