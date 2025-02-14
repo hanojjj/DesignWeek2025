cac374c245c12ee9bee98185d663680fce735727
Jonah Cappa <jonahcappaofficial@gmail.com>
2025-02-14 12:20:25 -0500

    last fixes

--- DesignWeek2025/Assets/Scripts/gameUI.cs
+++ DesignWeek2025/Assets/Scripts/gameUI.cs
@@ -17,13 +17,13 @@ public class gameUI : MonoBehaviour
     void Start()
     {
         time.text = "Time: " + death.timeRemaining;
-        score.text = death.P1Score.Value + " - " + death.P2Score.Value;
+        score.text = death.P2Score.Value + " - " + death.P1Score.Value;
     }
 
     // Update is called once per frame
     void Update()
     {
         time.text = "Time: " + death.timeRemaining.ToString("F1");
-        score.text = death.P1Score.Value + " - " + death.P2Score.Value;
+        score.text = death.P2Score.Value + " - " + death.P1Score.Value;
     }
 }
